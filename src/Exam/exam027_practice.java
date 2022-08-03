03af6541ad74716db74ea5cdc81953d082bb7963
Daniel_dev <clwnssla1@bible.ac.kr>
2022-08-03 17:36:11 +0900

    27번 문제 2022-8/3

--- src/Exam/exam027_practice.java
+++ src/Exam/exam027_practice.java
@@ -4,11 +4,11 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class exam027 {
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static boolean[][] visited;
+public class exam027_practice {
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int[][] A;
+    static boolean[][] visited;
     static int N, M;
 
     public static void main(String[] args) {
@@ -17,27 +17,28 @@ public class exam027 {
         M = sc.nextInt();
         A = new int[N][M];
         visited = new boolean[N][M];
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) A[i][j] = sc.nextInt();
         }
-        BFS(0, 0);
+        DFS(0, 0);
         System.out.println(A[N - 1][M - 1]);
     }
 
-    private static void BFS(int i, int j) {
+    private static void DFS(int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {i, j}); //큐에 현 위치의 좌표 삽입
+        queue.add(new int[] {i, j});
         visited[i][j] = true;
         while (!queue.isEmpty()) {
-            int now[] = queue.poll();
+            int[] now = queue.poll();
             for (int k = 0; k < 4; k++) {
-                int x = now[0] + dx[k]; // 좌우 검사
-                int y = now[1] + dy[k]; // 상하 검사
+                int x = now[0] + dx[k];
+                int y = now[1] + dy[k];
                 if (x >= 0 && y >= 0 && x < N && y < M) {
-                    if (A[x][y] != 0 && !visited[x][y]) { // 뚫려있는 미로이고, 방문한 적이 없다면
-                        visited[x][y] = true; // 방문배열 체크
-                        A[x][y] += A[now[0]][now[1]]; // 깊이 카운트 ++
-                        queue.add(new int[] {x, y}); // 큐에 삽입
+                    if (!visited[x][y] && A[x][y] != 0) {
+                        visited[x][y] = true;
+                        queue.add(new int[] {x, y});
+                        A[x][y] += A[now[0]][now[1]];
                     }
                 }
             }
