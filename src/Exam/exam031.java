207142bcba1ea3765180251b489247c887bf9bb8
Deniel_dev <clwnssla1@bible.ac.kr>
2022-08-09 17:55:42 +0900

    8/9

--- src/Exam/exam031.java
+++ src/Exam/exam031.java
@@ -1,7 +1,19 @@
 package Exam;
 
+import java.util.Scanner;
+
 public class exam031 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int k = sc.nextInt();
+        int[][] A = new int[N + 1][N + 1];
+        int startIndex = 1;
+        int endIndex = k;
 
+        while (startIndex <= endIndex) {
+            int mid = (startIndex + endIndex) / 2;
+            int count = 0;
+        }
     }
 }
